ef52b5f7bfbc2f6b33adbfbae129a79d6ffbfa1a
Dowoco <37535745+dowoco@users.noreply.github.com>
2023-06-27 11:55:10 +0100

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,54 @@
+import logging
+from pyzabbix import ZabbixMetric, ZabbixSender
+from flask import Flask, request
+
+app = Flask(__name__)
+
+gunicorn_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers = gunicorn_logger.handlers
+app.logger.setLevel(gunicorn_logger.level)
+
+@app.route('/metrics', methods=['POST'])
+def processMetric():
+    empty_values = 0
+
+    content_type = request.headers.get('Content-Type')
+
+    if (content_type == 'application/json'):
+       result=request.get_json()
+
+       if "server" not in result:
+          app.logger.error("Missing server parameter!")
+          empty_values += 1
+
+       if "host" not in result:
+          app.logger.error("Missing host parameter!")
+          empty_values += 1
+
+       if "key" not in result:
+          app.logger.error("Missing key parameter!")
+          empty_values += 1
+
+       if "value" not in result:
+          app.logger.error("Missing value parameter")
+          empty_values += 1
+
+       if empty_values == 0:
+          try:
+             packet = [
+                ZabbixMetric(str(result['host']), str(result['key']), str(result['value']))
+             ]
+             getvalues = ZabbixSender(str(result['server'])).send(packet)
+             #app.logger.info(getvalues)
+             if getvalues.processed == 1:
+                app.logger.info("Key Updated: " + str(result['key']) + " processed in: " + str(getvalues.time) + "s")
+                return 'Update Successful!'
+             else:
+                app.logger.error("Upload unsuccesseful!")
+                return 'Metric no Uploaded"!'
+          except:
+             app.logger.error("Unable to send data to Zabbix Server")
+             return 'Error Sending Data!'
+       else:
+          app.logger.error("Missing one or more Parameters!")
+          return 'Missing Parameters!'
